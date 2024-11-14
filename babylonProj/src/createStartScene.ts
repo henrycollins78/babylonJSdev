2e830d6cfa865e37638c65ffa7a266d9980cf1f9
Henry Collins <harrycollins528@gmail.com>
2024-11-14 20:36:29 +0000

    build

--- babylonProj/src/createStartScene.ts
+++ babylonProj/src/createStartScene.ts
@@ -1,5 +1,5 @@
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
+//import "@babylonjs/core/Debug/debugLayer";
+//import "@babylonjs/inspector";
 import {
     Scene,
     ArcRotateCamera,
@@ -73,12 +73,12 @@ import {
     }
   
     let that: SceneData = { scene: new Scene(engine) };
-    that.scene.debugLayer.show();
+    //that.scene.debugLayer.show();
   
-    that.box = createBox(that.scene);
-    that.light = createLight(that.scene);
-    that.sphere = createSphere(that.scene);
-    that.ground = createGround(that.scene);
-    that.camera = createArcRotateCamera(that.scene);
+    createBox(that.scene);
+    createLight(that.scene);
+    createSphere(that.scene);
+    createGround(that.scene);
+    createArcRotateCamera(that.scene);
     return that;
   }
